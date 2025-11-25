ee466d8648d2b2854128539e53e66513a70a0a90
m4rz3r0 <30533649+m4rz3r0@users.noreply.github.com>
2025-11-25 13:52:38 +0100

    refactor: renaming to match translations filename

--- crates/desktop/src/main.rs
+++ crates/desktop/src/main.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use desktop::launcher;
+use foruster_desktop::launcher;
 use std::error::Error;
 
 slint::include_modules!();
